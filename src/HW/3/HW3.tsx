acbba7a8c47a3ec936d04187b46ce8c430b8d68d
Uladzimir <boba70325@gmail.com>
2024-07-04 13:20:22 +0300

    Update "HW3.tsx"

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -16,12 +16,12 @@ export const HW3 = () => {
     'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
   ]);
 
-  const handleChange = (event:ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setCurrentText(event.currentTarget.value);
 
   };
 
-  const handleSave = (texts:string[]) => {
+  const handleSave = (texts: string[]) => {
     setTexts(texts)
     setCurrentText(" ")
     // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
@@ -30,17 +30,17 @@ export const HW3 = () => {
 
   return (
     <div id={'hw03'}>
-      {/*{currentText ? (*/}
-      {/*  <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>*/}
-      {/*) : (*/}
-      {/*  <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм*/}
-      {/*)}*/}
+      {currentText ?
+        (<h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>) :
+        (<h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм*/}
+        )}
 
-      {/*<input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />*/}
+      <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
 
-      {/*<button id={'hw03-button'} onClick={() => {}}> // НЕ ХВАТАТЕТ ФУНКЦИИ*/}
-      {/*  Сохранить*/}
-      {/*</button>*/}
+      <button id={'hw03-button'} onClick={() => { handleSave }}>
+        {/* НЕ ХВАТАТЕТ ФУНКЦИИ */}
+        {/*  Сохранить*/}
+      </button>
 
       {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
 
