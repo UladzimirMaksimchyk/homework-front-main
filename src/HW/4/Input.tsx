1f889605fe20704fbf640d65b7d3358df98cbe79
Uladzimir <boba70325@gmail.com>
2024-07-09 15:32:45 +0300

    Update "Input.tsx"

---
+++ src/HW/4/Input.tsx
@@ -0,0 +1,18 @@
+type InputPropsType = {
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText:(text:string)=>void // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Input = (props: InputPropsType) => {
+	const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		props.setCurrentText(event.target.value);
+		// НУЖНО ДОПИСАТЬ
+	};
+
+	return (
+	   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	 );
+};
+
+
+
