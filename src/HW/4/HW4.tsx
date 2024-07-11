e3b5526b6cef00f26a7eae3cdaa02ff6a3aa6426
Uladzimir <boba70325@gmail.com>
2024-07-11 18:05:27 +0300

    Update "HW4.tsx"

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -34,19 +34,19 @@ export const HW4 = () => {
 
       <Input currentText='111' setCurrentText={()=>{}}/>
       {/* НЕ ХВАТАЕТ АРГУМЕНТОВ */}
-      {/*<Button НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Button НЕ ХВАТАЕТ АРГУМЕНТОВ />
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw04-tasks'}>*/}
-      {/*  {texts.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={'hw04-tasks'}>
+      { {texts.map((el, index) => {
+          return (
+          <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>
+              {el}
+          </li>
+         );
+        })}
+      </ol>
     </div>
   );
 };
