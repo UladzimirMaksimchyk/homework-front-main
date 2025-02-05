64d72c094a337ee247d53721b77d804b88f10ba4
Uladzimir <boba70325@gmail.com>
2025-02-05 21:58:02 +0300

    Update "src/HW/4/HW4.tsx"

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -16,7 +16,7 @@ export const HW4 = () => {
 
   const [currentText, setCurrentText] = useState('');
   const [texts, setTexts] = useState<string[]>([
-    'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
+    'То, что вы делаете по ночам, то и делает вас богатым.',
   ]);
 
   const handleSave = () => {
